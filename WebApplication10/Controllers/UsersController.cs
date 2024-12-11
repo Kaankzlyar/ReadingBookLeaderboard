1b4d638da37328f7ee3e763552505b0469b85615
kaankzlyar <kaankzlyar@outlook.com>
2024-12-11 15:02:43 +0300

    LeaderboardFrontEnd

---
+++ WebApplication10/Controllers/UsersController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using LeaderboardFrontend.Models;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+
+namespace LeaderboardFrontend.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly string baseURL = "https://localhost:7172";
+
+        public async Task<IActionResult> Index()
+        {
+            List<BookItem> listUsers = new List<BookItem>();
+
+            using (var _httpClient = new HttpClient())
+            {
+                _httpClient.BaseAddress = new Uri(baseURL + "/api/BookItemsApi");
+                _httpClient.DefaultRequestHeaders.Accept.Clear();
+                _httpClient.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                // API'den verileri çekme
+                HttpResponseMessage getData = await _httpClient.GetAsync("");
+
+                if (getData.IsSuccessStatusCode)
+                {
+                    // Asenkron olarak içeriği oku ve deserialize et
+                    string result = await getData.Content.ReadAsStringAsync();
+                    listUsers = JsonConvert.DeserializeObject<List<BookItem>>(result);
+                }
+                else
+                {
+                    return RedirectToAction("ErrorPage"); // Hata sayfasına yönlendir
+                }
+            }
+
+            // Verileri View'e aktar
+            return View(listUsers);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> PostBookItem(BookItem log)
+        {
+            using (var _httpClient = new HttpClient())
+            {
+                _httpClient.BaseAddress = new Uri(baseURL + "/api/BookItemsApi");
+                _httpClient.DefaultRequestHeaders.Accept.Clear();
+                _httpClient.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+               
+                HttpResponseMessage postData = await _httpClient.PostAsJsonAsync("",log);
+
+                if (postData.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return RedirectToAction("ErrorPage"); // Hata sayfasına yönlendir
+                }
+            }
+        }
+
+        public IActionResult ErrorPage()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication10/Models/BookItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace LeaderboardFrontend.Models
+{
+    public class BookItem
+    {
+
+        public long Id { get; set; }
+        public int Rank { get; set; }
+        public string? UserName { get; set; }
+        public int PageNumber { get; set; }
+        public int TodayPageNumber { get; set; }
+        public int StreakNumber { get; set; }
+        public int TotalPages { get; set; } = 0;
+        public DateTime LastUpdate { get; set; } = DateTime.UtcNow;
+    }
+}
